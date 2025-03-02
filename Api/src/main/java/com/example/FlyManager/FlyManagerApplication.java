2984a111af857b3cf5d91c0fd6b2690e2ce3af18
andrusrem <andrusremets@gmail.com>
2025-03-02 13:19:10 +0200

    Add raw Api project

---
+++ Api/src/main/java/com/example/FlyManager/FlyManagerApplication.java
@@ -0,0 +1,13 @@
+package com.example.FlyManager;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlyManagerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FlyManagerApplication.class, args);
+	}
+
+}
---
+++ Api/src/test/java/com/example/FlyManager/FlyManagerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.FlyManager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FlyManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
