379abec2e8d06eeb921702cb4a4c583d6a477e7f
andrusrem <andrusremets@gmail.com>
2025-03-06 10:52:40 +0200

    Fix FlightChoose component

--- Front/src/FlightChoose.jsx
+++ Front/src/FlightChoose.jsx
@@ -2,37 +2,45 @@
 import axios from 'axios';
 import { useEffect, useState } from 'react';
 
+const getFlight = async (url, queryParams) => {
+    if (!url || !queryParams) {
+        console.error("Nullable body error!")
+    }
+    let request = await axios.get(url, queryParams);
+    console.log(request.data);
+    return request.data;
+}
 
-const FlightChoose = ({searchData}) => {
-    const {results, setResults} = useState({});
+const FlightChoose = ({ searchData }) => {
+    const { results, setResults } = useState({});
     var url = "http://localhost:8081/api/search";
-    const data = Object.entries(searchData);
-    let oneWay = false;
-    let direct = false;
-    console.log(data);
-    if( oneWay == "on")
-    {
-        oneWay = true;
-    }
-    if( direct == "on")
-    {
-        direct = true;
-    }
     useEffect(() => {
-        axios.get(url, {
-            "origin": data.origin,
-            "destination": data.destination,
-            "departureDate": data.departureDate,
-            "returnDate": data.returnDate,
-            "oneWay": data.oneWay,
-            "direct": data.direct,
-            "token": "example",
-        }).then((res)=> {
-            console.log(res.data);
+        if (searchData) {
+            let oneWay = false;
+            let direct = false;
+            if (searchData.oneWay == "on") {
+                oneWay = true;
+            }
+            if (searchData.direct == "on") {
+                direct = true;
+            }
+            const queryParams = {
+                "origin": searchData.origin,
+                "destination": searchData.destination,
+                "departureDate": searchData.departureDate,
+                "returnDate": searchData.returnDate,
+                "token": "example",
+                "oneWay": oneWay,
+                "direct": direct,
+                "sorting": "price",
+            };
+            console.log(queryParams);
+            getFlight(url, queryParams).then((res) => {
+                console.log(res);
+            })
         }
-        )
-    },[searchData])
-    
+    }, [searchData])
+
 }
 
 export default FlightChoose;
